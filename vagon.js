80e3d9a614460ce6d5b112afba7ccd8ea96808a1
E2395-Eren <2395-Eren@users.noreply.github.com>
2020-12-09 23:21:41 +0100

    son sürüm

--- vagon.js
+++ vagon.js
@@ -1,18 +1,17 @@
-let hak = 1;
-let i = 0;
+let hak = 4;
+let tahmin;
 let sayi = Math.round(Math.random() * 10);
-do {
-  let girdi = prompt("Tahmini Giriniz: "); // tahmin= parseInt(promp("tahmim giriniz: ")); şeklinde tek satırda yapılabilir.
-  let tahmin = parseInt(girdi);
+/* do {
+  //let girdi = ; // tahmin= parseInt(promp("tahmim giriniz: ")); şeklinde tek satırda yapılabilir.
+  let tahmin = Number(prompt("Tahmini Giriniz: "));
   if (tahmin < sayi) {
     console.log("Daha büyük bir sayı giriniz.<br>");
-  }
-  if (tahmin > sayi) {
+  } else if (tahmin > sayi) {
     console.log("Daha küçük bir sayı giriniz.<br>");
-  }
-  if (tahmin == sayi) {
+  } else if (tahmin == sayi) {
     console.log("Tebrikler bildiniz. Tuttuğum sayı: " + sayi + "<br>");
     console.log(hak + ". hakkınızda bildiniz.");
+    break;
   }
   hak++;
 } while (tahmin != sayi && hak <= 4);
@@ -20,4 +19,61 @@ do {
   if (hak > 4) {
     console.log("Hakkınız kalmadı. Tutulan sayı : " + sayi + "<br>");
   }
+} */
+/*let tahmin = Number(prompt("Tahmini Giriniz: "));
+while (tahmin != sayi) {
+  //let girdi = ; // tahmin= parseInt(promp("tahmim giriniz: ")); şeklinde tek satırda yapılabilir.
+
+  let tahmin = Number(prompt("Tahmini Giriniz: "));
+
+  if (tahmin < sayi) {
+    console.log("Daha büyük bir sayı giriniz.<br>");
+  } else if (tahmin > sayi) {
+    console.log("Daha küçük bir sayı giriniz.<br>");
+  } else if (tahmin == sayi) {
+    console.log("Tebrikler bildiniz. Tuttuğum sayı: " + sayi + "<br>");
+    console.log("bildiniz");
+    break;
+  }
+  hak--;
+  if (hak == 0) {
+    console.log("Hakkınız kalmadı. Tutulan sayı : " + sayi + "<br>");
+    break;
+  }
+}*/
+
+for (let i = 1; i <= hak; i++) {
+  //let girdi = ; // tahmin= parseInt(promp("tahmim giriniz: ")); şeklinde tek satırda yapılabilir.
+
+  let tahmin = Number(prompt("Tahmini Giriniz: "));
+  if (sayi == tahmin) {
+    console.log("bıldınız");
+    if (i == 1) {
+      console.log("aferin 100 aldın");
+    } else if (i == 2) {
+      console.log("aferin 75 aldın");
+    } else if (i == 3) {
+      console.log("aferin 50 aldın");
+    } else {
+      console.log("aferin 25 aldın");
+    }
+    break;
+  } else if (sayi > tahmin) {
+    console.log("büyük sayi giriniz");
+  } else if (sayi < tahmin) {
+    console.log("küçük sayi giriniz");
+  }
+  if (i == 4) {
+    console.log("hakkınız kalmadı");
+  }
+  /*  if (tahmin < sayi) {
+    console.log("Daha büyük bir sayı giriniz.<br>");
+  } else if (tahmin > sayi) {
+    console.log("Daha küçük bir sayı giriniz.<br>");
+  } else if (tahmin == sayi) {
+    console.log("Tebrikler bildiniz. Tuttuğum sayı: " + sayi + "<br>");
+    console.log("bildiniz");
+    break;
+  }
+  ; */
 }
